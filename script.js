54fe9afe769712177ea3870fed9fb1d2f51074a9
Pamella Freitas <pamellafreitas.dev@gmail.com>
2024-03-04 11:00:44 -0300

    Site avogada

---
+++ script.js
@@ -0,0 +1,41 @@
+const myObserver = new IntersectionObserver ((entries) => {
+
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+
+})
+
+const elements = document.querySelectorAll('.hide')
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: "start"
+    });
+
+    
+    if (navbarCollapse.classList.contains('show')) {
+        navbarToggler.click();
+    }
+
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+})
