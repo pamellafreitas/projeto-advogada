99e9c273b7ab8e951e7445f8d2bee8c07561e8bd
Pamella Freitas <pamellafreitas.dev@gmail.com>
2024-03-04 14:44:37 -0300

    correcao do scroll

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-const myObserver = new IntersectionObserver ((entries) => {
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
 
-    entries.forEach( (entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
-    })
+const elements = document.querySelectorAll('.hide');
 
-})
-
-const elements = document.querySelectorAll('.hide')
-
-elements.forEach((element) => myObserver.observe(element))
-
+elements.forEach((element) => myObserver.observe(element));
 
 const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarCollapse = document.querySelector('.navbar-collapse');
 
 function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  const nav = document.querySelector('nav');
 
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: "start"
-    });
+  const sectionTopPosition = section.getBoundingClientRect().top;
+  const navHeight = nav.getBoundingClientRect().height;
 
-    
-    if (navbarCollapse.classList.contains('show')) {
-        navbarToggler.click();
-    }
+  window.scroll({
+    top: window.scrollY + sectionTopPosition - navHeight,
+    behavior: 'smooth',
+  });
 
+  if (navbarCollapse.classList.contains('show')) {
+    navbarToggler.click();
+  }
 }
 
 linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-})
+  link.addEventListener('click', scrollToSection);
+});
